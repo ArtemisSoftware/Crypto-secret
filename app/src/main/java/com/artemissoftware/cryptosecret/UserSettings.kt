a62a86ac53aff689a6adadec790cfb5409183e25
gustavo <gustavo.isaac@gmail.com>
2023-03-26 15:05:28 +0100

    UserSettings

---
+++ app/src/main/java/com/artemissoftware/cryptosecret/UserSettings.kt
@@ -0,0 +1,9 @@
+package com.artemissoftware.cryptosecret
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserSettings(
+    val username: String? = null,
+    val password: String? = null,
+)
